635580b69b89f938c85135143f0f48e86609afac
yeelvus <50106230+yeelvus@users.noreply.github.com>
2026-01-10 13:03:37 +0700

    提交

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/lucky-draw' : '/'
+  // 将路径修改为 './'，这是打包桌面软件的关键
+  publicPath: './',
+  
+  // 如果你想关闭打包时的生产环境提示，可以加上这个（可选）
+  productionSourceMap: false
 };
